5138a6f768fa4b38eed03ddd5d102adeddc2615a
Ron Gross <ron.gross@gmail.com>
2017-07-19 18:42:01 +0300

    Connect to login API

--- Home.js
+++ Home.js
@@ -2,18 +2,33 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import Login from './components/Login/Login.js';
 
+// TODO Use prod server in prod mode 
+// const server = 'https://satoshi-api.herokuapp.com'
+const server = 'http://localhost:8080'
+
 export default class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = { text: null };
     }
 
-    onLogin = (nickname) => {
-        const password = 'TODO Password'
-        console.log(`Login: (${nickname}, ${password})`) // user credentials
-        switch (nickname) {
-            case 'ron':
-            case 'sharon':
+    onLogin = async (nickname) => {
+        const deviceId = 'TODO DeviceID'
+        console.log(`Login: (${nickname}, ${deviceId})`) // user credentials
+        let response = await fetch(`${server}/api/login`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                nickname: nickname,
+                deviceId: deviceId,
+            })
+        })
+
+        switch (response.json().status) {
+            case 'STATUS_OK':
                 this.props.navigation.navigate('Profile', { nickname: nickname })
                 return;
         }
